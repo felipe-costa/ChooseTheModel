2008300e5a747886e773c934fa6427e1b5074fe9
aub_fcosta <fscosta@aubay.com>
2019-10-26 13:57:00 +0100

    New files
    Change-Id: I60f45bfeea8a141cf2728c2141b378626545548a

---
+++ chooseTheModel/test.py
@@ -0,0 +1,12 @@
+from chooseTheModel import Chooser
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.svm import SVR
+import pandas as  pd
+import numpy as np
+import datetime
+
+now = datetime.datetime.now()
+print(now)
